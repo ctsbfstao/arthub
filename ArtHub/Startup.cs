84584e37726fdbebe51fc45024014280086a10f0
I00I <durgac.net@gmail.com>
2017-05-07 23:24:04 -0500

    inital commit

---
+++ ArtHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArtHub.Startup))]
+namespace ArtHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
